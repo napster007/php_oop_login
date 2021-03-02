1834105ebf74047be7d7295f256862759bcbcaf8
Jayson Olaguera <jaysonolaguera@gmail.com>
2021-03-03 00:50:18 +0800

    Editng login.php -adding a navbar

--- login.php
+++ login.php
@@ -7,13 +7,43 @@ include 'includes/autoload.inc.php';
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="components/bootstrap-5.0.0/css/bootstrap.min.css">
+    <script src="components/bootstrap-5.0.0/js/bootstrap.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+    <script src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
     <title>Login</title>
 </head>
 <body>
+
+    <nav class="navbar navbar-expand-md navbar-light bg-light sticky-top">
+        <div class="container-fluid">
+            <a href="#" class="navbar-brand"><img src="components/imgs/carousel/1.jpg" height="50px" width="70px"></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive">
+                <span class="navbar-toggler-icon"> </span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item active">
+                        <a href="#" class="nav-link active">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">About Us</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">Contact Us</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">Site Map</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
     <div>
         <form method="POST" action="includes/login.inc.php">
-            <label for="email">Email: </label><input type="email" name="uemail" id="uemail">
-            <label for="password">Password: </label> <input type="password" name="upassword" id="upassword">
+            <label for="email">Email: </label><input type="email" name="uemail" id="uemail" required>
+            <label for="password">Password: </label> <input type="password" name="upassword" id="upassword" required>
             <button type="submit">Login</button>
         </form>
     </div>

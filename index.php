f9a1d55ddd3e341e1e9800c3f374165cb8692199
Jayson Olaguera <jaysonolaguera@gmail.com>
2021-03-02 23:59:04 +0800

    modified carousel created in index.php

--- index.php
+++ index.php
@@ -8,8 +8,8 @@ include 'includes/autoload.inc.php';
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="components/bootstrap-4.6.0/css/bootstrap.css">
-    <link rel="stylesheet" type="text/css" href="components/bootstrap-4.6.0/css/bootstrap.min.css">
+ 
+    <link rel="stylesheet" type="text/css" href="components/bootstrap-5.0.0/css/bootstrap.min.css">
     <script src="components/bootstrap-4.6.0/js/bootstrap.min.js"></script>
     <script src="components/bootstrap-4.6.0/js/bootstrap.js"></script>
     <script src="components/js/jquery-3.5.1.min.js"></script>
@@ -18,7 +18,27 @@ include 'includes/autoload.inc.php';
     <title>Index</title>
 </head>
 <body>
-    <div class="container">
+    <style>
+        .carousel-item{
+          height:32rem;
+          background:#777;
+          color:white;
+          position:relative;
+        }
+
+        .container{
+         
+          padding-bottom:20px;
+        }
+
+        .carousel_container{
+            width:900px;
+            height:400px;
+            display:block;
+            margin:0 auto;
+        }
+      </style>
+      <div class="container">
         <div class="row">
             <div class="col-sm">
                 <ul class="nav justify-content-end ">
@@ -31,81 +51,69 @@ include 'includes/autoload.inc.php';
                 </ul>
             </div>
         </div>
+      </div>
 
-        <div class="row">
-            <!-- <div class="col-m">
-                bbody
-               
-            </div> -->
-        
-            <div class="col-m">
-                
-                <img src="..." class="rounded float-start" alt="...">
-                <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
-                    <div class="carousel-inner">
-                        <div class="carousel-item active" data-bs-interval="3000">
-                        <img src="components/imgs/carousel/1.jpg" class="d-block w-100" alt="...">
-                        </div>
-                        <div class="carousel-item" data-bs-interval="2000">
-                        <img src="components/imgs/carousel/2.jpg" class="d-block w-100" alt="...">
-                        </div>
-                        <div class="carousel-item">
-                        <img src="components/imgs/carousel/3.jpg" class="d-block w-100" alt="...">
-                        </div>
-                    </div>
-                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval"  data-bs-slide="prev">
-                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span class="visually-hidden">Previous</span>
-                    </button>
-                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval"  data-bs-slide="next">
-                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span class="visually-hidden">Next</span>
-                    </button>
-                </div>
-                
+ <div class="carousel_container">  
+    <div id="myCarousel" class="carousel slide" data-bs-ride="carousel">
+      <div class="carousel-indicators">
+        <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" class="active" aria-label="Slide 1"></button>
+        <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1" aria-label="Slide 2" class="" aria-current="true"></button>
+        <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="2" aria-label="Slide 3" class=""></button>
+      </div>
+      <div class="carousel-inner">
+        <div class="carousel-item" data-bs-interval="3000">
+          <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#777"></rect></svg>
+  
+          <div class="container">
+            <div class="carousel-caption">
+              <h1>Example headline.</h1>
+              <p>Some representative placeholder content for the first slide of the carousel.</p>
+              <p><a class="btn btn-lg btn-primary" href="#">Sign up today</a></p>
             </div>
-
-
-            <!-- <div class="col-m">
-
-                <figure class="figure">
-                    <img src="..." class="figure-img img-fluid rounded" alt="...">
-                    <figcaption class="figure-caption text-end">A caption for the above image.</figcaption>
-                </figure>
-            </div> -->
-
-
-
-          <!--   <div class="col-m">
-                
-                
-                <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
-                    <div class="carousel-inner">
-                        <div class="carousel-item active" data-bs-interval="3000">
-                        <img src="components/imgs/carousel/1.jpg" height="400px" width="800px" class="d-block w-100" alt="...">
-                        </div>
-                        <div class="carousel-item" data-bs-interval="2000">
-                        <img src="components/imgs/carousel/2.jpg" height="400px" width="800px" class="d-block w-100" alt="...">
-                        </div>
-                        <div class="carousel-item">
-                        <img src="components/imgs/carousel/3.jpg" height="400px" width="800px" class="d-block w-100" alt="...">
-                        </div>
-                    </div>
-                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval"  data-bs-slide="prev">
-                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span class="visually-hidden">Previous</span>
-                    </button>
-                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval"  data-bs-slide="next">
-                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span class="visually-hidden">Next</span>
-                    </button>
-                </div>
-                
-            </div> -->
-
+          </div>
         </div>
-        
-       
+        <div class="carousel-item active" data-bs-interval="2000">
+          <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#777"></rect></svg>
+  
+          <div class="container">
+            <div class="carousel-caption">
+              <h1>Another example headline.</h1>
+              <p>Some representative placeholder content for the second slide of the carousel.</p>
+              <p><a class="btn btn-lg btn-primary" href="#">Learn more</a></p>
+            </div>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#777"></rect></svg>
+  
+          <div class="container">
+            <div class="carousel-caption ">
+              <h1>One more for good measure.</h1>
+              <p>Some representative placeholder content for the third slide of this carousel.</p>
+              <p><a class="btn btn-lg btn-primary" href="#">Browse gallery</a></p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <button class="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+      </button>
+      <button class="carousel-control-next" type="button" data-bs-target="#myCarousel" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+      </button>
+    </div>
+
+</div>   
+
+
+
+
+
+
+    <!-- For carousel-->    
+        <script src="components/bootstrap-5.0.0/js/bootstrap.bundle.min.js" ></script>   
         
     </div>
 </body>
